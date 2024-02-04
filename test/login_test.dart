668544083c548d1bdbfead7f9c20503274495a53
varun <varuntej07.cv@gmail.com>
2024-02-04 13:40:00 -0800

    created tests for the widgets in Login page

---
+++ test/login_test.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pawfect_match_app/login.dart';
+
+void main(){
+  testWidgets("Testing Widgets in Login page", (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: Login(),
+    ));
+
+    expect(find.byType(TextField), findsNWidgets(2));
+    expect(find.byType(ElevatedButton), findsExactly(1));
+    expect(find.byType(TextButton), findsExactly(1));
+
+    await tester.enterText(find.byType(TextField).at(0), "varuntej07");
+    await tester.enterText(find.byType(TextField).at(1), "weakpassword");
+
+    expect(find.text("varuntej07"), findsOneWidget);
+    expect(find.text("weakpassword"), findsOneWidget);
+
+    //await tester.tap(find.byType(ElevatedButton));
+  });
+}
