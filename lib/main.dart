2a1ad32ca1575e43ac3bfc67a7a2989521abf024
onalanb <baran.onalan@gmail.com>
2024-02-03 17:27:29 -0800

    Had to roll back code, Varun's push made us lose our previous implementation. Please make sure the code is functional before pushing to main.
    Signed-off-by: onalanb <baran.onalan@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,24 @@
-// Baran
+// Baran Onalan
+// February 6th, 2024
+// CPSC 5350 Mobile Development Group Project
+
+// Importing the necessary packages for the project.
 import 'package:flutter/material.dart';
+import 'create_profile.dart';
 
 void main() {
-  runApp(const MaterialApp(home: HomePage()));
+  runApp(PawfectMatchApp());
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
+class PawfectMatchApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Welcome to Pawfect!'),
+    return MaterialApp(
+      title: 'Pawfect Match App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [_logIn(), _signUp()],
-        ),
-      ),
-    );
-  }
-
-  Widget _logIn() {
-    return Column(
-      children: [
-        TextField(
-          decoration: InputDecoration(
-              hintText: "Username",
-              border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20.0))),
-        ),
-        const SizedBox(height: 8),
-        TextField(
-          decoration: InputDecoration(
-              hintText: "Password",
-              border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20.0))),
-        ),
-        const SizedBox(height: 8),
-        ElevatedButton(
-            onPressed: () {},
-            child: const Text(
-              "Login",
-              style: TextStyle(fontSize: 20),
-            )
-        ),
-        const Text("Don't have an account? Sign Up")
-      ],
-    );
-  }
-
-  Widget _signUp() {
-    return const Column(
-      children: <Widget>[
-        TextField(
-          decoration: InputDecoration(
-            hintText: "Enter your name",
-          ),
-        ),
-        SizedBox(height: 8),
-        TextField(
-          decoration: InputDecoration(
-            hintText: "Enter your Dog's Breed",
-          ),
-        ),
-        SizedBox(height: 8),
-        Row(
-          children: [
-            Expanded(
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: "Age ",
-                ),
-              ),
-            ),
-            SizedBox(width: 8),
-            Expanded(
-              child: TextField(
-                decoration: InputDecoration(labelText: "Weight "),
-              ),
-            ),
-          ],
-        ),
-      ],
+      home: ProfileCreationPage(),
     );
   }
 }
