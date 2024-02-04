d073018b9e27b8b207a1e00d4df6be49cdab58bd
varun <varuntej07.cv@gmail.com>
2024-02-04 14:28:05 -0800

    created basic Widget tests in Signup page

---
+++ test/create_profile_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pawfect_match_app/create_profile.dart';
+
+void main(){
+  testWidgets("Testing whether the user be able to see and enter info ", (WidgetTester tester) async{
+    await tester.pumpWidget(const MaterialApp(
+        home: ProfileCreationPage()
+    ));
+
+    expect(find.byType(TextField), findsNWidgets(7));
+    expect(find.byType(ElevatedButton), findsNWidgets(3));
+
+    await tester.enterText(find.byType(TextField).at(0), "varuntej07");
+    await tester.enterText(find.byType(TextField).at(1), "weakpassword");
+    await tester.enterText(find.byType(TextField).at(2), "Snoopy");
+    await tester.enterText(find.byType(TextField).at(3), "Rottweiler");
+    await tester.enterText(find.byType(TextField).at(4), "Pissing off people");
+
+    expect(find.text("varuntej07"), findsOneWidget);
+    expect(find.text("weakpassword"), findsOneWidget);
+    expect(find.text("Snoopy"), findsOneWidget);
+    expect(find.text("Rottweiler"), findsOneWidget);
+    expect(find.text("Pissing off people"), findsOneWidget);
+  });
+}
