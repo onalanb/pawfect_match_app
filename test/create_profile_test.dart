a7c5cebfbcfbc738380119c5ef45f3198afad16d
onalanb <baran.onalan@gmail.com>
2024-02-29 22:56:52 -0800

    This is the unit test for our create profile page.
    THE FINAL SOLUTION CREDIT FOR THIS UNIT TEST GOES TO JAMES
    
    Signed-off-by: onalanb <baran.onalan@gmail.com>

--- test/create_profile_test.dart
+++ test/create_profile_test.dart
@@ -1,39 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:pawfect_match_app/create_profile.dart';
+import 'package:pawfect_match_app/user_repo.dart';
 
-void main(){
-  testWidgets("Testing SignUp page for users to be able to enter info ", (WidgetTester tester) async{
-    await tester.pumpWidget(const MaterialApp(
-        home:ProfileCreationPage(dbPath: '')
+class MockUserRepository extends Mock implements UserRepository {}
+
+void main() {
+  testWidgets("Testing SignUp page for users to be able to enter info", (WidgetTester tester) async {
+    final mockUserRepository = MockUserRepository();
+    await tester.pumpWidget(MaterialApp(
+      home: ProfileCreationPage(userRepository: mockUserRepository),
     ));
-
     expect(find.byType(Scaffold), findsOneWidget);
-
     expect(find.byType(AppBar), findsOneWidget);
     expect(find.text('Create Profile'), findsOneWidget);
-
     expect(find.byType(TextFormField), findsNWidgets(7));
     expect(find.byType(ElevatedButton), findsNWidgets(3));
+    await tester.enterText(find.byType(TextFormField).at(0), "varuntej07"); // username
+    await tester.enterText(find.byType(TextFormField).at(1), "weakpassword"); // password
+    await tester.enterText(find.byType(TextFormField).at(2), "Snoopy"); // dog's name
+    await tester.enterText(find.byType(TextFormField).at(3), "Rottweiler"); // dog's breed
+    await tester.enterText(find.byType(TextFormField).at(4), "2"); // dog's age
+    await tester.enterText(find.byType(TextFormField).at(5), "Pissing off people"); // about the dog
+    await tester.enterText(find.byType(TextFormField).at(6), "911"); // phone number
 
-    const String age = '2';
-    const String contact = '911';
-
-    await tester.enterText(find.byType(TextFormField).at(0), "varuntej07");
-    await tester.enterText(find.byType(TextFormField).at(1), "weakpassword");
-    await tester.enterText(find.byType(TextFormField).at(2), "Snoopy");
-    await tester.enterText(find.byType(TextFormField).at(3), "Rottweiler");
-    await tester.enterText(find.byType(TextFormField).at(4), contact);
-    await tester.enterText(find.byType(TextFormField).at(5), age);
-    await tester.enterText(find.byType(TextFormField).at(6), "Pissing off people");
-
-
+    await tester.tap(find.byType(DropdownButtonFormField<String>));
+    await tester.pumpAndSettle();
+    var genderDropdownItem = find.text('Male').last;
+    await tester.tap(genderDropdownItem);
+    await tester.pumpAndSettle();
     expect(find.text("varuntej07"), findsOneWidget);
     expect(find.text("weakpassword"), findsOneWidget);
     expect(find.text("Snoopy"), findsOneWidget);
     expect(find.text("Rottweiler"), findsOneWidget);
-    expect(find.text(contact), findsOneWidget);
-    expect(find.text(age), findsOneWidget);
+    expect(find.text("2"), findsOneWidget);
     expect(find.text("Pissing off people"), findsOneWidget);
+    expect(find.text("911"), findsOneWidget);
+    expect(find.text('Male'), findsWidgets);
   });
 }
